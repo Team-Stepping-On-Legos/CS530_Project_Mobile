5ed0728cbe18c2aa98b8ada79408afb59c444a12
Mansimran Singh <mansimran.singh@codecorp.com>
2021-09-23 08:59:00 -0400

    Feature added - Firebase Cloud Messaging to display notifications for 'cat1' on when app is in background

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:cs530_mobile/views/splash.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
@@ -7,6 +8,21 @@ void main() async {
   await Firebase.initializeApp().whenComplete(() {
     print("Completed intializing Firebase Core");
   });
+
+  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+  print('Got a message whilst in the foreground!');
+  print('Message data: ${message.data}');
+
+  if (message.notification != null) {
+    print('Message also contained a notification: ${message.notification}');
+  }
+});
+
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+  
+  // subscribe to topic on each app start-up
+  await FirebaseMessaging.instance.subscribeToTopic('cat1');
+
   runApp(const MyApp());
 }
 
@@ -25,4 +41,13 @@ class MyApp extends StatelessWidget {
       home: const SplashScreen(),
     );
   }
+}
+
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  // If you're going to use other Firebase services in the background, such as Firestore,
+  // make sure you call `initializeApp` before using other Firebase services.
+  await Firebase.initializeApp();
+
+  print("Handling a background message: ${message.messageId}");
+  print(message.notification.toString());
 }
